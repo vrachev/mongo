3255ae58c1f57225fcc7e1bc58e0807bd2a6ec21
vrachev <vlad.rachev@mongodb.com>
2019-08-07 12:44:40 -0400

    a

--- src/mongo/db/sorter/sorter.cpp
+++ src/mongo/db/sorter/sorter.cpp
@@ -45,7 +45,6 @@
  *
  * Do this once for each unique set of parameters to MONGO_CREATE_SORTER.
  */
-#define MONGO_LOG_DEFAULT_COMPONENT ::mongo::logger::LogComponent::kReplication
 
 #include "mongo/util/log.h"
 #include "mongo/db/sorter/sorter.h"
