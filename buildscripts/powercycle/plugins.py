6dc074d260fe9fb7be8e177229755e53bf5ad9aa
vrachev <vlad.rachev@mongodb.com>
2020-06-26 13:15:29 -0400

    run hang-analyzer on remote machines

--- buildscripts/powercycle/plugins.py
+++ buildscripts/powercycle/plugins.py
@@ -375,6 +375,53 @@ class CopyEC2MonitorFiles(PowercycleCommand):
         self.remote_op.operation(SSHOperation.SHELL, cmd, None)
         self.remote_op.operation(SSHOperation.COPY_FROM, 'ec2_monitor_files.tgz', None)
 
+class RunHangAnalyzerOnRemoteInstance(PowercycleCommand):
+    """Run the hang-analyzer on a remote instance."""
+    COMMAND = "runHangAnalyzerOnRemoteInstance"
+
+    def execute(self) -> None:
+        """
+        :return: None
+        """
+        if "private_ip_address" not in self.expansions:
+            return
+        hang_analyzer_processes = "dbtest,java,mongo,mongod,mongos,python,_test" if "hang_analyzer_processes" not in self.expansions else self.expansions["hang_analyzer_processes"]
+        hang_analyzer_option = f"-o file -o stdout -p {hang_analyzer_processes}"
+        hang_analyzer_dump_core = True if "hang_analyzer_dump_core" not in self.expansions else self.expansions["hang_analyzer_dump_core"]
+        if hang_analyzer_dump_core:
+            hang_analyzer_option = f"-c {hang_analyzer_option}"
+
+        core_ext = "core"
+        if self.is_windows():
+            core_ext = "mdmp"
+        remote_dir = "." if "remote_dir" not in self.expansions else self.expansions["remote_dir"]
+        debug_files = self._call("ls *.debug *.dSYM *.pdb")[1].split("\n")
+        file_param = []
+        for debug_file in debug_files:
+            file_param.append(debug_file)
+        if file_param:
+            self.remote_op.operation(SSHOperation.COPY_TO, file_param, remote_dir)
+
+        # Activate virtualenv on remote host. The virtualenv bin_dir is different for Linux and
+        # Windows.
+        venv = "venv" if "virtualenv_dir" not in self.expansions else self.expansions["virtual_env_dir"]
+        virtual_env = os.environ(["VIRTUAL_ENV"])
+        _, activate_loc = self._call(f"find {virtual_env} -name activate")
+        _, bin_dir = self.call(f"sed -e \"s,{virtual_env},,;s,activate,,;s,/,,g\" {activate_loc}")
+        cmds = f". {venv}/{bin_dir}/activate"
+        # In the 'cmds' variable we pass to remote host, use 'python' instead of '$python' since
+        # we don't want to evaluate the local python variable, but instead pass the python string
+        # so the remote host will use the right python when the virtualenv is sourced.
+        cmds = f"{cmds}; cd {remote_dir}"
+        cmds = f"{cmds}; PATH=\"/opt/mongodbtoolchain/gdb/bin:$PATH\" python buildscripts/resmoke.py hang-analyzer {hang_analyzer_option}"
+        self.remote_op.operation(SSHOperation.SHELL, cmds, None)
+
+        file_param = []
+        file_param.append(f"{remote_dir}/debugger*.*")
+        file_param.append(f"{remote_dir}/*.{core_ext}")
+        self.remote_op.operation(SSHOperation.COPY_TO, file_param, None)
+
+
 class PowercyclePlugin(PluginInterface):
     """Interact with UndoDB."""
 
--- buildscripts/powercycle/run.py
+++ buildscripts/powercycle/run.py
@@ -134,7 +134,7 @@ class RemoteOperations(object):  # pylint: disable=too-many-instance-attributes
         """
 
         if not self.access_established():
-            return self.access_info()
+            print("Return code: {} for command {}".format(self.access_info()[0], self.access_info()[1]))
 
         # File names with a space must be quoted, since we permit the
         # the file names to be either a string or a list.
